4589fca15cd1aadb30108d48d81f23ae57581d82
Fabio Vilhena <fvilhena@gmail.com>
2020-12-07 23:46:11 -0300

    Aula 6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description, :due_date, :done)
+    params.require(:task).permit(:description, :due_date, :done, :parent_id)
   end
 
 end
